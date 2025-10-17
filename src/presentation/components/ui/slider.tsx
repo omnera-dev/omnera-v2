39b73f8367217d74a099357e1336aa9f9408487a
Thomas JEANNEAU <thomas.jeanneau@latechforce.com>
2025-10-17 02:47:52 +0200

    revert npm version

--- src/presentation/components/ui/slider.tsx
+++ src/presentation/components/ui/slider.tsx
@@ -21,7 +21,7 @@ function Slider({
   ...props
 }: Readonly<React.ComponentProps<typeof SliderPrimitive.Root>>) {
   const _values = React.useMemo(
-    () => (Array.isArray(value) ? value : (Array.isArray(defaultValue) ? defaultValue : [min, max])),
+    () => (Array.isArray(value) ? value : Array.isArray(defaultValue) ? defaultValue : [min, max]),
     [value, defaultValue, min, max]
   )
 
